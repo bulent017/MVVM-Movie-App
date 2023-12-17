05dea4a9f659501ef7070be1f25508b3c1ab65d2
bulentoral <96498139+bulent017@users.noreply.github.com>
2023-12-17 16:31:46 +0300

    Ekranlar yapıldı

--- app/src/main/java/com/bulentoral/movieapp/MainActivity.kt
+++ app/src/main/java/com/bulentoral/movieapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.bulentoral.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.bulentoral.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navcontroller: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navcontroller = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navcontroller)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navcontroller.navigateUp() || super.onSupportNavigateUp()
     }
 }
